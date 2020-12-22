08db4432fc50576c6b1553848d6184cc0b6f5c6d
Artyom Golenishchev <48377936+heywannafunk@users.noreply.github.com>
2020-12-22 20:48:42 +0300

    Downloaded all the files
    Added all the python code of this task

---
+++ bt11.py
@@ -0,0 +1,59 @@
+# 11. [10 баллов] Напишите программу, которая считывает из файла описание грамматики, удаляет из нее недостижимые
+# символы и выводит текст с описанием новой грамматики. Грамматику описывать как в предыдущем задании.
+import re
+
+file = open("grammar2.txt", "r")
+grammarData = file.readline()
+file.close()
+#print(grammarData)
+
+print()
+print("исходная грамматика содержит следующие правила:")
+grammarRules = grammarData.split(" ")
+for rule in grammarRules:
+    print(rule)
+
+yi = []
+nonterminalSybmols = []
+for rule in grammarRules:
+    nonterminalSybmols.append(rule[0])
+yi.append(set(nonterminalSybmols[0])) # записываем стартовый символ во множество достижимых
+yi.append(set())
+
+#print(nonterminalSybmols)
+#print(yi)
+
+i = 1
+while not (yi[i-1] == yi[i-2]):
+    for symbol in yi[i-1]:
+        for rule in grammarRules:
+            if rule.startswith(symbol):
+                for char in rule:
+                    if char != '-' and char != '>' and char != '|':
+                        yi[i].add(char)
+    yi.append(set())
+    i=i+1
+
+reachableSymbols = yi[-2]
+
+#print(reachableSymbols)
+
+newNonterminals = set(nonterminalSybmols).intersection(set(reachableSymbols))
+newGrammarRules = []
+for symbol in newNonterminals:
+    for rule in grammarRules:
+        if rule.startswith(symbol):
+            newGrammarRules.append(rule)
+
+#print(newNonterminals)
+#print(newGrammarRules)
+
+print()
+print("новая грамматика содержит следующие правила: ")
+for rule in newGrammarRules:
+    print(rule)
+
+
+
+
+
